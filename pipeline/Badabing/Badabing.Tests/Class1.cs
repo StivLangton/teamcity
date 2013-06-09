cf9594d125cbdcc2e40d48ba711d6b0d56f00716
Hadi Hariri <mail@hadihariri.com>
2013-06-09 19:05:39 +0200

    added pipeline

---
+++ pipeline/Badabing/Badabing.Tests/Class1.cs
@@ -0,0 +1,28 @@
+﻿
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Badabing.Tests
+{
+    [TestClass]
+    public class Class1
+    {
+        [TestMethod]
+        public void AllTests()
+        {
+            
+        }
+
+        [TestMethod]
+        public void Test1()
+        {
+            
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            
+        }
+    }
+}
